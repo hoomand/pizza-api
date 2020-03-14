99a9b0a15765bb591fe3812c9a8e30e1b7676e9a
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-03-14 18:53:57 +1100

    fix(UpdateOrder): remove need for address in order update

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -4,8 +4,8 @@ const updateOrderPizza = (orderId, newOrder) => {
   if (!orderId) {
     throw Error("Order ID must be specified");
   }
-  if (!newOrder || !newOrder.pizzaId || !newOrder.address) {
-    throw Error("Pizza id and address are needed for an order");
+  if (!newOrder || !newOrder.pizzaId) {
+    throw Error("Pizza id is needed to identify an order");
   }
 
   const { pizzaId, address } = order;
